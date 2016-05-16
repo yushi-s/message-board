d90aa7d6551679749335b9bff79afd11355c9d49
yushi <tachikomano007@gmail.com>
2016-05-16 08:52:23 +0000

    add age column

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,6 @@ class Message < ActiveRecord::Base
   validates :name, length: { maximum: 20 }, presence: true
   # 内容は必須入力かつ2文字以上30文字以内
   validates :body, length: { minimum: 2, maximum: 30 }, presence: true
+
+  validates :age, numericality: { only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 150 }, presence: true
 end
